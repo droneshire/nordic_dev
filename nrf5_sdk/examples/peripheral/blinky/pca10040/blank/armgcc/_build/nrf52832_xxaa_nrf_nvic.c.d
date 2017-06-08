7d53498163b74ebbd9d8294c0c8c4216ef8240b1
rossyeager <ross@boostedboards.com>
2017-06-08 00:00:48 -0700

    Initial Commit

---
+++ nrf5_sdk/examples/peripheral/blinky/pca10040/blank/armgcc/_build/nrf52832_xxaa_nrf_nvic.c.d
@@ -0,0 +1,104 @@
+_build/nrf52832_xxaa_nrf_nvic.c.o: \
+ ../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_nvic.c \
+ /opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/stdlib.h \
+ /opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/machine/ieeefp.h \
+ /opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/_ansi.h \
+ /opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/newlib.h \
+ /opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/_newlib_version.h \
+ /opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/sys/config.h \
+ /opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/sys/features.h \
+ /opt/gcc/gcc-arm-none-eabi-6-2017/lib/gcc/arm-none-eabi/6.3.1/include/stddef.h \
+ /opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/sys/reent.h \
+ /opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/_ansi.h \
+ /opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/sys/_types.h \
+ /opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/machine/_types.h \
+ /opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/machine/_default_types.h \
+ /opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/sys/lock.h \
+ /opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/sys/cdefs.h \
+ /opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/machine/stdlib.h \
+ ../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_soc.h \
+ /opt/gcc/gcc-arm-none-eabi-6-2017/lib/gcc/arm-none-eabi/6.3.1/include/stdint.h \
+ /opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/stdint.h \
+ /opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/sys/_intsup.h \
+ /opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/sys/_stdint.h \
+ ../../../../../../components/device/nrf.h \
+ ../../../../../../components/device/nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../../components/toolchain/system_nrf52.h \
+ ../../../../../../components/device/nrf52_bitfields.h \
+ ../../../../../../components/device/nrf51_to_nrf52.h \
+ ../../../../../../components/device/nrf52_name_change.h \
+ ../../../../../../components/device/compiler_abstraction.h \
+ ../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h
+
+/opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/stdlib.h:
+
+/opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/machine/ieeefp.h:
+
+/opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/_ansi.h:
+
+/opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/newlib.h:
+
+/opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/_newlib_version.h:
+
+/opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/sys/config.h:
+
+/opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/sys/features.h:
+
+/opt/gcc/gcc-arm-none-eabi-6-2017/lib/gcc/arm-none-eabi/6.3.1/include/stddef.h:
+
+/opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/sys/reent.h:
+
+/opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/_ansi.h:
+
+/opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/sys/_types.h:
+
+/opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/machine/_types.h:
+
+/opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/machine/_default_types.h:
+
+/opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/sys/lock.h:
+
+/opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/sys/cdefs.h:
+
+/opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/machine/stdlib.h:
+
+../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_soc.h:
+
+/opt/gcc/gcc-arm-none-eabi-6-2017/lib/gcc/arm-none-eabi/6.3.1/include/stdint.h:
+
+/opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/stdint.h:
+
+/opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/sys/_intsup.h:
+
+/opt/gcc/gcc-arm-none-eabi-6-2017/arm-none-eabi/include/sys/_stdint.h:
+
+../../../../../../components/device/nrf.h:
+
+../../../../../../components/device/nrf52.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cm4.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmInstr.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmFunc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmSimd.h:
+
+../../../../../../components/toolchain/system_nrf52.h:
+
+../../../../../../components/device/nrf52_bitfields.h:
+
+../../../../../../components/device/nrf51_to_nrf52.h:
+
+../../../../../../components/device/nrf52_name_change.h:
+
+../../../../../../components/device/compiler_abstraction.h:
+
+../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h:
